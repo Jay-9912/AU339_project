5b4af3f3846b06c389c601535b4c70247c7a5f54
jaywu <1799010336@qq.com>
2021-12-18 00:50:49 +0800

    add visualization for sight

--- README.md
+++ README.md
@@ -7,12 +7,23 @@
 
     Python 3.7, gym
 
-3. To-do list
+3. Installation
+
+    We modify the ***gym*** module for some additional function. Concretely, you should add the function below to ***class Viewer*** in ***rendering.py***.
+
+    ```python
+        def set_color_rgbd(self, r, g, b, d): 
+            self._color.vec4 = (r, g, b, d)
+    ```
+
+4. To-do list
 
     - [x] multi guards
     - [x] scalable map
     - [x] thief model
     - [x] alert on theft event and perception of other guards nearby
+    - [x] visualize the sight of guards
+    - [x] add cartoon images to guards and thieves
     - [ ] patrolling algorithm
     - [ ] dispatching algorithm
 
--- my_env.py
+++ my_env.py
@@ -12,6 +12,9 @@ from gym.envs.classic_control import rendering
 
 logger = logging.getLogger(__name__)
 
+GRAPH_LENGTH = 40
+GRAPH_WIDTH = 36
+
 # gym中显示的方格与序号关系，以3*4(l*w)为例
 # 9 10 11
 # 6  7  8
@@ -20,19 +23,53 @@ logger = logging.getLogger(__name__)
 
 
 class Agent():
-    def __init__(self, init_state, id):
+    def __init__(self, init_state, id, res):
         self.state = init_state
         self.id = id
         self.guard_nearby = []
+        self.observe_range = 3
+        self.res = res
+
+    def change(self):
+        self.viz.set_color(1, 0, 0)
 
     def render(self):
-        self.viz = rendering.make_circle(6)
+        self.viz = rendering.Image('guard.jpg', self.res - 1, self.res - 1)
         self.trans = rendering.Transform()
         self.viz.add_attr(self.trans)
-        self.viz.set_color(0, 1, 0)
+        self.sight_list = {}
+        self.trans_list = {}
+        for i in range(-self.observe_range, self.observe_range + 1):
+            for j in range(
+                    abs(i) - self.observe_range,
+                    self.observe_range - abs(i) + 1):
+                if i == 0 and j == 0:
+                    continue
+                sight = rendering.make_polygon([(0, 0), (self.res, 0),
+                                                (self.res, self.res),
+                                                (0, self.res)])
+                trans = rendering.Transform()
+                sight.add_attr(trans)
+                self.trans_list[(i, j)] = trans
+                sight.set_color_rgbd(112 / 255.0, 10 / 255.0, 245 / 255.0, 0.2)
+                self.sight_list[(i, j)] = sight
 
     def set_location(self, x, y):
         self.trans.set_translation(x, y)
+        for key in self.sight_list.keys():
+            row = self.state // GRAPH_LENGTH
+            col = self.state % GRAPH_LENGTH
+            row_sight = row + key[0]
+            col_sight = col + key[1]
+            self.trans_list[key].set_translation(
+                x + key[1] * self.res - self.res / 2,
+                y + key[0] * self.res - self.res / 2)
+            if row_sight < 0 or row_sight >= GRAPH_WIDTH or col_sight < 0 or col_sight >= GRAPH_LENGTH:
+                self.sight_list[key].set_color_rgbd(112 / 255.0, 10 / 255.0,
+                                                    245 / 255.0, 0)
+            else:
+                self.sight_list[key].set_color_rgbd(112 / 255.0, 10 / 255.0,
+                                                    245 / 255.0, 0.2)
 
     def get_state(self):
         return self.state
@@ -59,16 +96,16 @@ class Agent():
 
 
 class Thief():
-    def __init__(self, init_state, id):
+    def __init__(self, init_state, id, res):
         self.state = init_state
         self.id = id
         self.escape = 0
+        self.res = res
 
     def render(self):
-        self.viz = rendering.make_circle(6)
+        self.viz = rendering.Image('thief.jpg', self.res - 1, self.res - 1)
         self.trans = rendering.Transform()
         self.viz.add_attr(self.trans)
-        self.viz.set_color(1, 0, 0)
 
     def set_location(self, x, y):
         self.trans.set_translation(x, y)
@@ -81,9 +118,10 @@ class Thief():
 
     def __del__(self):
         if self.escape == 0:
-            print("Thief No.%d is caught by guard"%(self.id))
+            print("Thief No.%d is caught by guard" % (self.id))
         else:
-            print("Thief No.%d escapes"%(self.id))
+            print("Thief No.%d escapes" % (self.id))
+
 
 class School(gym.Env):
     metadata = {
@@ -91,7 +129,8 @@ class School(gym.Env):
         'video.frames_per_second': 1
     }
 
-    def __init__(self, graph, agent_num, perception_range, thief_num, theft_time, obstacle_num):
+    def __init__(self, graph, agent_num, perception_range, thief_num,
+                 theft_time, obstacle_num):
         self.set_seed(4)
         self.graph = graph  # rect_graph
         self.grid_size = 20
@@ -99,7 +138,7 @@ class School(gym.Env):
         self.agent_num = agent_num
         self.thief_num = thief_num
         self.obstacle_num = obstacle_num
-        self.perception_range =perception_range
+        self.perception_range = perception_range
         self.theft_time = theft_time
         self.agent_list = []
         self.thief_list = {}
@@ -154,7 +193,7 @@ class School(gym.Env):
     def init_agent(self):
         # 初始化agent
         for num in range(self.agent_num):
-            agent = Agent(self.state[num], num)
+            agent = Agent(self.state[num], num, self.grid_size)
             self.agent_list.append(agent)
 
     def generate_thief(self):
@@ -167,19 +206,19 @@ class School(gym.Env):
                                     size=self.thief_num,
                                     replace=False)
         for id, loc in enumerate(loc_list):
-            thief = Thief(loc_list[id], id)
+            thief = Thief(loc_list[id], id, self.grid_size)
             thief.render()
             self.viewer.add_geom(thief.viz)
             thief.set_location(self.x[loc], self.y[loc])
             self.thief_list[id] = thief
-            
+
     def cal_loss(self):
         keys = list(self.thief_list.keys())
         self.loss += len(keys)
         for k in keys:
-            self.thief_list[k].escape = 1 # 逃跑成功
-            self.viewer.geoms.remove(self.thief_list[k].viz) # 删除图形显示
-            self.thief_list.pop(k) # 删除键
+            self.thief_list[k].escape = 1  # 逃跑成功
+            self.viewer.geoms.remove(self.thief_list[k].viz)  # 删除图形显示
+            self.thief_list.pop(k)  # 删除键
 
     def set_seed(self, seed):
         random.seed(seed)
@@ -266,8 +305,8 @@ class School(gym.Env):
             agent.clear_guard_nearby()
         for i in range(self.agent_num):
             for j in range(i + 1, self.agent_num):
-                if self.cal_state_dist(self.agent_list[i].get_state(),
-                                       self.agent_list[j].get_state()) < self.perception_range:
+                if self.cal_state_dist(self.agent_list[i].get_state(
+                ), self.agent_list[j].get_state()) < self.perception_range:
                     self.agent_list[i].add_guard_nearby(j)
                     self.agent_list[j].add_guard_nearby(i)
         # for agent in self.agent_list:
@@ -277,9 +316,10 @@ class School(gym.Env):
         for agent in self.agent_list:
             keys = list(self.thief_list.keys())
             for k in keys:
-                if self.cal_state_dist(agent.get_state(), self.thief_list[k].get_state()) <= 1:
-                    self.viewer.geoms.remove(self.thief_list[k].viz) # 删除图形显示
-                    self.thief_list.pop(k) # 删除键
+                if self.cal_state_dist(agent.get_state(),
+                                       self.thief_list[k].get_state()) <= 1:
+                    self.viewer.geoms.remove(self.thief_list[k].viz)  # 删除图形显示
+                    self.thief_list.pop(k)  # 删除键
 
         return next_state_list
 
@@ -312,6 +352,8 @@ class School(gym.Env):
             for agent in self.agent_list:
                 agent.render()
                 self.viewer.add_geom(agent.viz)
+                for key in agent.sight_list.keys():
+                    self.viewer.add_geom(agent.sight_list[key])
 
             for i in self.lines:
                 self.viewer.add_geom(i)
@@ -348,15 +390,15 @@ class School(gym.Env):
 
 
 if __name__ == '__main__':
-    g = Rect_Graph(40, 36)
+    g = Rect_Graph(GRAPH_LENGTH, GRAPH_WIDTH)
     env = School(g, 4, 5, 6, 10, 8)
     env.reset()
 
     for i in range(1000):
         # 假设2s时出现小偷，10s偷完，这里需要根据需要设置合适的小偷产生模型
-        if i == 2: 
+        if i == 2:
             env.generate_thief()
-        if i == 10: 
+        if i == 10:
             env.cal_loss()
             print('loss:', env.loss)
         # if i ==3 :
